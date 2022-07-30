6b538402345513abb0de90c7014d1162e7a2bbdf
maxs2x <62260902+maxs2x@users.noreply.github.com>
2022-07-31 00:39:35 +0300

    slider block frame

--- src/pages/mainPage/index.js
+++ src/pages/mainPage/index.js
@@ -3,3 +3,5 @@ import './index.scss';
 import '../../components/header/header.js';
 import '../../components/offer/offer.js';
 import '../../components/feedbackForm/feedbackForm.js';
+import '../../components/offer/offer.js';
+import '../../components/refinement-slider/refinement-slider.js';
